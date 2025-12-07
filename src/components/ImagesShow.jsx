7c557b7665f5f823e0f15a2a41985d4f31d56067
Derangula-Harish-Kumar <dharishk8@gmail.com>
2025-12-07 18:45:11 +0530

    Adding Images sections

---
+++ src/components/ImagesShow.jsx
@@ -0,0 +1,35 @@
+
+
+export function ImagesShow(){
+    return(
+        <>
+        <div className='fle flex-wrap items-center content-center w-full mt-[3vw] p-5 lg:p-15  '>
+            
+            <div className="relative text-center flex items-center w-[100%] shadow-lg p-4 py-10 bg-[#D2FCED] rounded-2xl mb-[3vw]">
+              <p className="pl-5 font-bold font-Charm sm:text-lg md:text-xl lg:text-5xl">Mandapam Decoration</p>
+              <a href="#" className="absolute right-5 text-blue-500 font-extrabold text-[] hover:underline">click hear..</a>
+            </div>
+
+            <div className="relative text-center flex items-center w-full shadow-lg p-4 py-10 bg-[#FEE5EC] rounded-2xl mb-[3vw]">
+              <p className="pl-5 font-bold font-Charm Noto-Serif sm:text-lg md:text-xl lg:text-5xl">Wedding Decorations</p>
+              <a href="#" className="absolute right-5 text-blue-500 font-extrabold hover:underline">click hear..</a>
+            </div>
+
+            <div className="relative text-center flex items-center w-full shadow-lg p-4 py-10 bg-[#FEF7C3] rounded-2xl mb-[3vw]">
+              <p className="pl-5 font-bold font-Charm sm:text-lg md:text-xl lg:text-5xl">Haldi Decoration</p>
+              <a href="#" className="absolute right-5 text-blue-500 font-extrabold hover:underline">click hear..</a>
+            </div>
+
+            <div className="relative text-center flex items-center w-full shadow-lg p-4 py-10 bg-[#c8eaf8] rounded-2xl mb-[3vw]">
+              <p className="pl-5 font-bold font-Charm sm:text-lg md:text-xl lg:text-5xl">Pandiri Designs</p>
+              <a href="#" className="absolute right-5 text-blue-500 font-extrabold hover:underline">click hear..</a>
+            </div>
+
+            <div className="relative text-center flex items-center w-full shadow-lg p-4 py-10 bg-[#77aba4] rounded-2xl">
+              <p className="pl-5 font-bold font-Charm sm:text-lg md:text-xl lg:text-5xl">Birthday Partys</p>
+              <a href="#" className="absolute right-5 text-blue-500 font-extrabold hover:underline">click hear..</a>
+            </div>
+        </div>
+        </>
+    )
+}
