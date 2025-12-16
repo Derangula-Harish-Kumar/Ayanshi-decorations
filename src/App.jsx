b3543d07922e42c580b9e0ed48d89c7e8a3bd0f3
Derangula-Harish-Kumar <dharishk8@gmail.com>
2025-12-16 23:24:01 +0530

    Fix: Refactor to data router to support ScrollRestoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-
 import React from 'react';
-import { Routes, Route, ScrollRestoration } from 'react-router-dom';
-import Decoration from './Decoration.jsx';
-import PageWeddingDecorations from './Pages/PageWeddingDecorations.jsx';
-import MandapamDecorationPage from './Pages/MandapamDecorationPage.jsx';
-import HaldiDecorationPage from './Pages/HaldiDecorationPage.jsx';
-import PandiriDesignsPage from './Pages/PandiriDesignsPage.jsx';
-import BirthdayPartysPage from './Pages/BirthdayPartysPage.jsx';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx';
 
 const App = () => {
@@ -14,17 +7,9 @@ const App = () => {
     <>
       <NavBar />
       <ScrollRestoration />
-      <Routes>
-        <Route path="/" element={<Decoration />} />
-        <Route path="/wedding-decorations" element={<PageWeddingDecorations />} />
-        <Route path="/mandapam-decoration" element={<MandapamDecorationPage />} />
-        <Route path="/haldi-decoration" element={<HaldiDecorationPage />} />
-        <Route path="/pandiri-designs" element={<PandiriDesignsPage />} />
-        <Route path="/birthday-partys" element={<BirthdayPartysPage />} />
-      </Routes>
+      <Outlet />
     </>
   );
 };
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Decoration from './Decoration.jsx'
+import PageWeddingDecorations from './Pages/PageWeddingDecorations.jsx'
+import MandapamDecorationPage from './Pages/MandapamDecorationPage.jsx'
+import HaldiDecorationPage from './Pages/HaldiDecorationPage.jsx'
+import PandiriDesignsPage from './Pages/PandiriDesignsPage.jsx'
+import BirthdayPartysPage from './Pages/BirthdayPartysPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Decoration />,
+      },
+      {
+        path: "/wedding-decorations",
+        element: <PageWeddingDecorations />,
+      },
+      {
+        path: "/mandapam-decoration",
+        element: <MandapamDecorationPage />,
+      },
+      {
+        path: "/haldi-decoration",
+        element: <HaldiDecorationPage />,
+      },
+      {
+        path: "/pandiri-designs",
+        element: <PandiriDesignsPage />,
+      },
+      {
+        path: "/birthday-partys",
+        element: <BirthdayPartysPage />,
+      },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
